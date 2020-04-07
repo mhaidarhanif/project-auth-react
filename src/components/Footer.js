7fae962b9eff586f4e3f0876d5c9f2088246c464
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 20:21:26 +0800

    Style Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import styled from '@xstyled/emotion'
+
+const FooterStyled = styled.footer`
+  text-align: center;
+  padding: 1rem;
+  text-transform: uppercase;
+  background: #111;
+  color: #fd3;
+`
 
 const Footer = () => {
   return (
-    <footer>
+    <FooterStyled>
       <small>2020 &copy; Project Auth React</small>
-    </footer>
+    </FooterStyled>
   )
 }
 
