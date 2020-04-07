5ee10e5de09ce51ab054cb8e3d6838b2c18c3050
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 19:28:58 +0800

    Refactor App component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+import Users from './components/Users'
+
 const App = () => {
-  return <div>Project Auth React</div>
+  return (
+    <div>
+      <h1>Project Auth React</h1>
+      <Users></Users>
+    </div>
+  )
 }
 
 export default App
