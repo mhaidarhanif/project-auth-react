5043eac4063f41fd72c8c42a39a741d8889449d7
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 21:30:48 +0800

    Refactor Form subcomponents into their own

---
+++ src/components/Form.js
@@ -0,0 +1,63 @@
+import styled from '@xstyled/emotion'
+
+export const Form = styled.form`
+  background: #eee;
+  padding: 2rem;
+  border-radius: 0.5rem;
+  min-width: 500px;
+  display: flex;
+  flex-direction: column;
+`
+
+export const FormGroup = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Label = styled.label`
+  font-size: 1.2em;
+  padding: 0.5em 0;
+  font-weight: 700;
+`
+
+export const LabelError = styled.span`
+  color: red;
+  font-weight: 400;
+`
+
+export const Input = styled.input`
+  font-size: 1.2em;
+  padding: 1em;
+  border-radius: 0.5rem;
+  border: 3px solid #aaa;
+  &:focus {
+    outline: none;
+    border-color: #fd3;
+  }
+`
+
+export const SubmitButton = styled.input`
+  background-color: hsl(50, 100%, 60%);
+  border: none;
+  border-radius: 0.5rem;
+  color: #000;
+  font-size: 1.2em;
+  padding: 1rem 2rem;
+  text-align: center;
+  text-decoration: none;
+  margin: 1rem 0;
+  cursor: pointer;
+  transition-property: background-color;
+  transition-duration: 0.5s;
+
+  &:hover {
+    background-color: hsl(50, 100%, 40%);
+  }
+  &:focus {
+    background-color: hsl(50, 100%, 30%);
+  }
+`
+
+export const FormHelp = styled.p`
+  text-align: center;
+`
--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,71 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
-import styled from '@xstyled/emotion'
 
 import { emailPattern } from '../utils/regex'
-
-const Form = styled.form`
-  background: #eee;
-  padding: 2rem;
-  border-radius: 0.5rem;
-  min-width: 500px;
-  display: flex;
-  flex-direction: column;
-`
-
-const FormGroup = styled.div`
-  display: flex;
-  flex-direction: column;
-`
-
-const Label = styled.label`
-  font-size: 1.2em;
-  padding: 0.5em 0;
-  font-weight: 700;
-`
-
-const LabelError = styled.span`
-  color: red;
-  font-weight: 400;
-`
-
-const Input = styled.input`
-  font-size: 1.2em;
-  padding: 1em;
-  border-radius: 0.5rem;
-  border: 3px solid #aaa;
-  &:focus {
-    outline: none;
-    border-color: #fd3;
-  }
-`
-
-const SubmitButton = styled.input`
-  background-color: hsl(50, 100%, 60%);
-  border: none;
-  border-radius: 0.5rem;
-  color: #000;
-  font-size: 1.2em;
-  padding: 1rem 2rem;
-  text-align: center;
-  text-decoration: none;
-  margin: 1rem 0;
-  cursor: pointer;
-  transition-property: background-color;
-  transition-duration: 0.5s;
-
-  &:hover {
-    background-color: hsl(50, 100%, 40%);
-  }
-  &:focus {
-    background-color: hsl(50, 100%, 30%);
-  }
-`
-
-const FormHelp = styled.p`
-  text-align: center;
-`
+import {
+  Form,
+  FormGroup,
+  Label,
+  LabelError,
+  Input,
+  SubmitButton,
+  FormHelp,
+} from './Form'
 
 const RegisterForm = () => {
   const { register, handleSubmit, errors } = useForm()
