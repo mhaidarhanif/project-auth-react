c4d649665a0a19ddebfe5bbd33515bde6ab6b6e6
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-07 19:55:25 +0800

    Implement asynchronous users fetching with hook
    Fix problem which unsubscribe API call

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,36 +1,55 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import fetch from '../utils/fetch'
 
 const Users = () => {
-  const [users, setUsers] = useState([])
+  const [users, setUsers] = useState()
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const CancelToken = axios.CancelToken
+    const source = CancelToken.source()
+
+    const loadData = async () => {
       try {
-        const response = await fetch('/users')
+        const response = await fetch.get('/users', {
+          cancelToken: source.token,
+        })
         const users = response.data.data
         setUsers(users)
       } catch (error) {
-        console.error(error)
+        if (axios.isCancel(error)) {
+          console.log('cancelled')
+        } else {
+          throw error
+        }
       }
     }
-    fetchUsers()
-  })
+
+    loadData()
+
+    return () => {
+      source.cancel()
+    }
+  }, [])
 
   return (
     <div>
       <h2>List of Users</h2>
-      <div>
-        {users.map((user, index) => {
-          return (
-            <div key={index}>
-              <h3>{user.name}</h3>
-              <p>{user.email}</p>
-            </div>
-          )
-        })}
-      </div>
+      {!users ? (
+        <div>Loading users...</div>
+      ) : (
+        <div>
+          {users.map((user, index) => {
+            return (
+              <div key={index}>
+                <h3>{user.name}</h3>
+                <p>{user.email}</p>
+              </div>
+            )
+          })}
+        </div>
+      )}
     </div>
   )
 }
