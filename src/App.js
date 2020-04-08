9ade21d6653204b90657f5d53cea1c2274cf4c56
M Haidar Hanif <git@mhaidarhanif.com>
2020-04-08 20:34:56 +0800

    Check connection to backend when opened

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import HomePage from './pages/HomePage'
 import RegisterPage from './pages/RegisterPage'
+import RegisterSuccessPage from './pages/RegisterSuccessPage'
 import LoginPage from './pages/LoginPage'
 import DashboardPage from './pages/DashboardPage'
 import UsersPage from './pages/UsersPage'
 import SearchPage from './pages/SearchPage'
+import LogoutPage from './pages/LogoutPage'
+
+import fetch from './utils/fetch'
 
 const App = () => {
+  // Check connection whether the Internet and backend is alive
+  useEffect(() => {
+    const CancelToken = axios.CancelToken
+    const source = CancelToken.source()
+    const checkConnection = async () => {
+      try {
+        await fetch.get('/', {
+          cancelToken: source.token,
+        })
+        console.log('Connection is online')
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          console.error('cancelled')
+        } else {
+          console.error('Connection is offline')
+          console.error(error)
+        }
+      }
+    }
+    checkConnection()
+    return () => {
+      source.cancel()
+    }
+  }, [])
+
   return (
     <Router>
       <div>
@@ -17,8 +46,11 @@ const App = () => {
           <Route path='/' exact>
             <HomePage />
           </Route>
-          <Route path='/register'>
+          <Route path='/register' exact>
             <RegisterPage />
+          </Route>
+          <Route path='/register/success'>
+            <RegisterSuccessPage />w
           </Route>
           <Route path='/login'>
             <LoginPage />
@@ -31,6 +63,9 @@ const App = () => {
           </Route>
           <Route path='/search'>
             <SearchPage />
+          </Route>
+          <Route path='/logout'>
+            <LogoutPage />
           </Route>
         </Switch>
       </div>
